0502ea77886c4f9b434a17cb10c01e5b987c7885
Mo <mc.xia@modemacbook-pro.local>
2018-03-05 18:41:13 -0500

    Initial Commit

---
+++ No Food Left Behind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  No Food Left Behind
+//
+//  Created by Mo on 2018/3/5.
+//  Copyright © 2018年 Mo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
